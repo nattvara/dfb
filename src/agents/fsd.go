80ad1ff7b621d1ef036ca9bcba2ef4bc5f6680f2
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2019-07-22 12:07:29 +0200

    restore fsds run every two second behavior

--- src/agents/fsd.go
+++ src/agents/fsd.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 )
 
 func main() {
@@ -17,46 +18,48 @@ func main() {
 		os.Exit(0)
 	}()
 
-	groups := g.FetchGroups()
-	groupsMounted := g.NumberOfGroupsMounted(groups)
+	for range time.Tick(2 * time.Second) {
+		groups := g.FetchGroups()
+		groupsMounted := g.NumberOfGroupsMounted(groups)
 
-	for _, group := range groups {
+		for _, group := range groups {
 
-		domains := group.Domains()
-		for _, domain := range domains {
-			if !domain.IsSymlinkedDomain() {
-				continue
-			}
-			if domain.Symlink.Availible() {
-				domain.Symlink.LinkDomainToProxyIfNotLinked()
-			} else {
-				domain.Symlink.UnlinkDomainFromProxyIfLinked()
-			}
-		}
-
-		if group.IsMounted() {
+			domains := group.Domains()
 			for _, domain := range domains {
-				domain.CreatePathIfNotCreated()
-				if !domain.LinkToBackupsExist() {
-					log.Printf("[domain: %s] has no link to backups, creating", domain.Name)
-					domain.CreateLinkToBackups(group.Mountpoint())
-				}
-			}
-		} else if groupsMounted == 0 {
-			for _, domain := range domains {
-				if domain.LinkToBackupsExist() {
-					log.Printf("[domain: %s] has link to backups, removing", domain.Name)
-					domain.DeleteLinkToBackups(group.Mountpoint())
-				}
-				if !domain.WritablePathExists() {
+				if !domain.IsSymlinkedDomain() {
 					continue
 				}
-				if domain.IsEmpty() && domain.IsTemporary() {
-					log.Printf("[domain: %s] removing temporary path", domain.Name)
-					domain.DeletePath()
-				} else if domain.IsTemporary() {
-					log.Printf("[domain: %s] removing temporary flag", domain.Name)
-					domain.DeleteTemporaryFlag()
+				if domain.Symlink.Availible() {
+					domain.Symlink.LinkDomainToProxyIfNotLinked()
+				} else {
+					domain.Symlink.UnlinkDomainFromProxyIfLinked()
+				}
+			}
+
+			if group.IsMounted() {
+				for _, domain := range domains {
+					domain.CreatePathIfNotCreated()
+					if !domain.LinkToBackupsExist() {
+						log.Printf("[domain: %s] has no link to backups, creating", domain.Name)
+						domain.CreateLinkToBackups(group.Mountpoint())
+					}
+				}
+			} else if groupsMounted == 0 {
+				for _, domain := range domains {
+					if domain.LinkToBackupsExist() {
+						log.Printf("[domain: %s] has link to backups, removing", domain.Name)
+						domain.DeleteLinkToBackups(group.Mountpoint())
+					}
+					if !domain.WritablePathExists() {
+						continue
+					}
+					if domain.IsEmpty() && domain.IsTemporary() {
+						log.Printf("[domain: %s] removing temporary path", domain.Name)
+						domain.DeletePath()
+					} else if domain.IsTemporary() {
+						log.Printf("[domain: %s] removing temporary flag", domain.Name)
+						domain.DeleteTemporaryFlag()
+					}
 				}
 			}
 		}
