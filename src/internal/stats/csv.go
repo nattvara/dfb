850fc777015529a9ded2fdfab85e33ed028f4bf4
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2019-07-28 19:16:22 +0200

    add features to stats package for parsing a csv into an in memory database stats.DB

---
+++ src/internal/stats/csv.go
@@ -0,0 +1,81 @@
+package stats
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"time"
+)
+
+// csvReadSummaries reads given csv file, parses and returns snapshot summaries
+func csvReadSummaries(filename string) []*SnapshotSummary {
+	f, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	var summaries []*SnapshotSummary
+
+	reader := csv.NewReader(f)
+	lineNumber := -1
+	for {
+		line, err := reader.Read()
+		lineNumber++
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			if err, ok := err.(*csv.ParseError); ok && err.Err == csv.ErrFieldCount {
+				fmt.Printf("delete line %v\n", lineNumber)
+				continue
+			}
+		}
+
+		filesNew, _ := strconv.Atoi(line[1])
+		filesChanged, _ := strconv.Atoi(line[2])
+		filesUnmodified, _ := strconv.Atoi(line[3])
+		dirsNew, _ := strconv.Atoi(line[4])
+		dirsChanged, _ := strconv.Atoi(line[5])
+		dirsUnmodified, _ := strconv.Atoi(line[6])
+		dataBlobs, _ := strconv.Atoi(line[7])
+		treeBlobs, _ := strconv.Atoi(line[8])
+		dataAdded, _ := strconv.Atoi(line[9])
+		totalFilesProcessed, _ := strconv.Atoi(line[10])
+		totalBytesProcessed, _ := strconv.Atoi(line[11])
+		totalDuration, _ := strconv.ParseFloat(line[12], 64)
+		date, _ := time.Parse("2006-01-02T15:04:05Z0700", line[17])
+
+		summary := &SnapshotSummary{
+			FilesNew:            filesNew,
+			FilesChanged:        filesChanged,
+			FilesUnmodified:     filesUnmodified,
+			DirsNew:             dirsNew,
+			DirsChanged:         dirsChanged,
+			DirsUnmodified:      dirsUnmodified,
+			DataBlobs:           dataBlobs,
+			TreeBlobs:           treeBlobs,
+			DataAdded:           dataAdded,
+			TotalFilesProcessed: totalFilesProcessed,
+			TotalBytesProcessed: totalBytesProcessed,
+			TotalDuration:       totalDuration,
+
+			SnapshotID: line[13],
+			Group:      line[14],
+			Domain:     line[15],
+			Repo:       line[16],
+
+			GroupWithWildcard:  []string{line[14], AllDomains},
+			DomainWithWildcard: []string{line[15], AllDomains},
+			DateString:         date.Format(getDateLayoutForTimeUnit("day")),
+			MonthString:        date.Format(getDateLayoutForTimeUnit("month")),
+			YearString:         date.Format(getDateLayoutForTimeUnit("year")),
+		}
+		summaries = append(summaries, summary)
+	}
+
+	return summaries
+}
---
+++ src/internal/stats/db.go
@@ -0,0 +1,151 @@
+package stats
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/hashicorp/go-memdb"
+	"github.com/nattvara/dfb/src/internal/paths"
+)
+
+// SnapshotSummary is based on a message emitted by restic
+// when the backup command finishes
+type SnapshotSummary struct {
+	// Metadata
+	SnapshotID string
+	Group      string
+	Domain     string
+	Repo       string
+
+	// Additional fields used for querying
+	GroupWithWildcard  []string
+	DomainWithWildcard []string
+	DateString         string
+	MonthString        string
+	YearString         string
+
+	// Values used for metrics
+	FilesNew            int
+	FilesChanged        int
+	FilesUnmodified     int
+	DirsNew             int
+	DirsChanged         int
+	DirsUnmodified      int
+	DataBlobs           int
+	TreeBlobs           int
+	DataAdded           int
+	TotalFilesProcessed int
+	TotalBytesProcessed int
+	TotalDuration       float64
+}
+
+// DB is a database wrapper
+//
+// Leveraging hashicorp/go-memdb it provides features to load
+// memdb with backup data from csv files for a given group,
+// and retrieve object by querying various indices
+type DB struct {
+	memdb *memdb.MemDB
+}
+
+// NewDB returns a new DB instance
+func NewDB() *DB {
+	schema := &memdb.DBSchema{
+		Tables: map[string]*memdb.TableSchema{
+			"snapshot": &memdb.TableSchema{
+				Name: "snapshot",
+				Indexes: map[string]*memdb.IndexSchema{
+					"id": &memdb.IndexSchema{
+						Name:    "id",
+						Unique:  true,
+						Indexer: &memdb.StringFieldIndex{Field: "SnapshotID"},
+					},
+					"repo_group_domain_daily": &memdb.IndexSchema{
+						Name:   "repo_group_domain_daily",
+						Unique: false,
+						Indexer: &memdb.CompoundMultiIndex{
+							AllowMissing: false,
+							Indexes: []memdb.Indexer{
+								&memdb.StringFieldIndex{Field: "Repo"},
+								&memdb.StringSliceFieldIndex{Field: "GroupWithWildcard"},
+								&memdb.StringSliceFieldIndex{Field: "DomainWithWildcard"},
+								&memdb.StringFieldIndex{Field: "DateString"},
+							},
+						},
+					},
+					"repo_group_domain_monthly": &memdb.IndexSchema{
+						Name:   "repo_group_domain_monthly",
+						Unique: false,
+						Indexer: &memdb.CompoundMultiIndex{
+							AllowMissing: false,
+							Indexes: []memdb.Indexer{
+								&memdb.StringFieldIndex{Field: "Repo"},
+								&memdb.StringSliceFieldIndex{Field: "GroupWithWildcard"},
+								&memdb.StringSliceFieldIndex{Field: "DomainWithWildcard"},
+								&memdb.StringFieldIndex{Field: "MonthString"},
+							},
+						},
+					},
+					"repo_group_domain_yearly": &memdb.IndexSchema{
+						Name:   "repo_group_domain_yearly",
+						Unique: false,
+						Indexer: &memdb.CompoundMultiIndex{
+							AllowMissing: false,
+							Indexes: []memdb.Indexer{
+								&memdb.StringFieldIndex{Field: "Repo"},
+								&memdb.StringSliceFieldIndex{Field: "GroupWithWildcard"},
+								&memdb.StringSliceFieldIndex{Field: "DomainWithWildcard"},
+								&memdb.StringFieldIndex{Field: "YearString"},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	memdb, err := memdb.NewMemDB(schema)
+	if err != nil {
+		panic(err)
+	}
+
+	return &DB{
+		memdb: memdb,
+	}
+}
+
+// Load loads db with data from csv files for given group
+func (db *DB) Load(groupName string) {
+	statsDir := fmt.Sprintf("%s/%s/stats", paths.DFB(), groupName)
+
+	summaries := csvReadSummaries(statsDir + "/snapshots.csv")
+	db.InsertSnapshotSummaries(summaries)
+}
+
+// InsertSnapshotSummaries inserts memdb instance with snapshot summaries
+func (db *DB) InsertSnapshotSummaries(summaries []*SnapshotSummary) {
+	txn := db.memdb.Txn(true)
+	for _, snapshot := range summaries {
+		if err := txn.Insert("snapshot", snapshot); err != nil {
+			panic(err)
+		}
+	}
+	txn.Commit()
+}
+
+// GetIndexFromTimeUnit returns the index to use for given time unit
+func (db *DB) GetIndexFromTimeUnit(timeUnit string) string {
+	var index string
+	switch strings.ToLower(timeUnit) {
+	case "day":
+		index = "repo_group_domain_daily"
+	case "month":
+		index = "repo_group_domain_monthly"
+	case "year":
+		index = "repo_group_domain_yearly"
+	default:
+		log.Fatal("unsupported timeUnit: " + timeUnit)
+	}
+	return index
+}
