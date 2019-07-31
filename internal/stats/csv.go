99b17f694c41c123cbac42cb93adc27270ca9f21
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2019-07-31 23:03:07 +0200

    refactor 'read from csv' functions to use an iterator to loop through records in file

--- internal/stats/csv.go
+++ internal/stats/csv.go
@@ -9,45 +9,74 @@ import (
 	"time"
 )
 
+// csvFileIterator is a type that can iterate over the records in a csv file
+type csvFileIterator struct {
+	filename          string
+	file              *os.File
+	reader            *csv.Reader
+	CurrentLineNumber int
+}
+
+// Open opens csv file at given filename for csvFileIterator it
+func (it *csvFileIterator) Open(filename string) {
+	it.filename = filename
+
+	var err error
+	it.file, err = os.Open(filename)
+
+	if err != nil {
+		log.Fatal("failed to open csv file ", err)
+	}
+
+	it.reader = csv.NewReader(it.file)
+	it.CurrentLineNumber = -1
+}
+
+// Close closes file descriptor used for reading csv by csvFileIterator it
+func (it *csvFileIterator) Close() {
+	it.file.Close()
+}
+
+// Next reads and returns the next record from opened csv file by csvFileIterator it
+func (it *csvFileIterator) Next() []string {
+	it.CurrentLineNumber++
+	line, err := it.reader.Read()
+	if err == io.EOF {
+		return nil
+	}
+
+	if err != nil {
+		if err, ok := err.(*csv.ParseError); ok && err.Err == csv.ErrFieldCount {
+			log.Printf("%s parse error at line %v\n", it.filename, it.CurrentLineNumber)
+			return it.Next()
+		}
+	}
+
+	return line
+}
+
 // csvReadSummaries reads given csv file, parses and returns snapshot summaries
 func csvReadSummaries(filename string) []*SnapshotSummary {
-	f, err := os.Open(filename)
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-
 	var summaries []*SnapshotSummary
 
-	reader := csv.NewReader(f)
-	lineNumber := -1
-	for {
-		line, err := reader.Read()
-		lineNumber++
-		if err == io.EOF {
-			break
-		}
+	it := &csvFileIterator{}
+	it.Open(filename)
+	defer it.Close()
 
-		if err != nil {
-			if err, ok := err.(*csv.ParseError); ok && err.Err == csv.ErrFieldCount {
-				fmt.Printf("delete line %v\n", lineNumber)
-				continue
-			}
-		}
-
-		filesNew, _ := strconv.Atoi(line[1])
-		filesChanged, _ := strconv.Atoi(line[2])
-		filesUnmodified, _ := strconv.Atoi(line[3])
-		dirsNew, _ := strconv.Atoi(line[4])
-		dirsChanged, _ := strconv.Atoi(line[5])
-		dirsUnmodified, _ := strconv.Atoi(line[6])
-		dataBlobs, _ := strconv.Atoi(line[7])
-		treeBlobs, _ := strconv.Atoi(line[8])
-		dataAdded, _ := strconv.Atoi(line[9])
-		totalFilesProcessed, _ := strconv.Atoi(line[10])
-		totalBytesProcessed, _ := strconv.Atoi(line[11])
-		totalDuration, _ := strconv.ParseFloat(line[12], 64)
-		date, _ := time.Parse("2006-01-02T15:04:05Z0700", line[17])
+	for record := it.Next(); record != nil; record = it.Next() {
+		filesNew, _ := strconv.Atoi(record[1])
+		filesChanged, _ := strconv.Atoi(record[2])
+		filesUnmodified, _ := strconv.Atoi(record[3])
+		dirsNew, _ := strconv.Atoi(record[4])
+		dirsChanged, _ := strconv.Atoi(record[5])
+		dirsUnmodified, _ := strconv.Atoi(record[6])
+		dataBlobs, _ := strconv.Atoi(record[7])
+		treeBlobs, _ := strconv.Atoi(record[8])
+		dataAdded, _ := strconv.Atoi(record[9])
+		totalFilesProcessed, _ := strconv.Atoi(record[10])
+		totalBytesProcessed, _ := strconv.Atoi(record[11])
+		totalDuration, _ := strconv.ParseFloat(record[12], 64)
+		date, _ := time.Parse("2006-01-02T15:04:05Z0700", record[17])
 
 		summary := &SnapshotSummary{
 			FilesNew:            filesNew,
@@ -63,13 +92,13 @@ func csvReadSummaries(filename string) []*SnapshotSummary {
 			TotalBytesProcessed: totalBytesProcessed,
 			TotalDuration:       totalDuration,
 
-			SnapshotID: line[13],
-			Group:      line[14],
-			Domain:     line[15],
-			Repo:       line[16],
+			SnapshotID: record[13],
+			Group:      record[14],
+			Domain:     record[15],
+			Repo:       record[16],
 
-			GroupWithWildcard:  []string{line[14], AllDomains},
-			DomainWithWildcard: []string{line[15], AllDomains},
+			GroupWithWildcard:  []string{record[14], AllDomains},
+			DomainWithWildcard: []string{record[15], AllDomains},
 			DateString:         date.Format(getDateLayoutForTimeUnit(TimeUnitDays)),
 			MonthString:        date.Format(getDateLayoutForTimeUnit(TimeUnitMonths)),
 			YearString:         date.Format(getDateLayoutForTimeUnit(TimeUnitYears)),
@@ -82,39 +111,23 @@ func csvReadSummaries(filename string) []*SnapshotSummary {
 
 // csvReadRepoBackupTime reads given csv file, parses and returns repo backup times
 func csvReadRepoBackupTime(filename string) []*RepoBackupTime {
-	f, err := os.Open(filename)
-	defer f.Close()
-	if err != nil {
-		panic(err)
-	}
-
 	var backupTimes []*RepoBackupTime
 
-	reader := csv.NewReader(f)
-	lineNumber := -1
-	for {
-		line, err := reader.Read()
-		lineNumber++
-		if err == io.EOF {
-			break
-		}
+	it := &csvFileIterator{}
+	it.Open(filename)
+	defer it.Close()
 
-		if err != nil {
-			if err, ok := err.(*csv.ParseError); ok && err.Err == csv.ErrFieldCount {
-				log.Printf("%s parse error at line %v\n", filename, lineNumber)
-				continue
-			}
-		}
+	for record := it.Next(); record != nil; record = it.Next() {
 
-		duration, _ := strconv.ParseFloat(line[0], 64)
-		date, _ := time.Parse("2006-01-02T15:04:05Z0700", line[3])
+		duration, _ := strconv.ParseFloat(record[0], 64)
+		date, _ := time.Parse("2006-01-02T15:04:05Z0700", record[3])
 
 		bt := &RepoBackupTime{
 			Took: duration,
 
-			ID:    lineNumber,
-			Group: line[1],
-			Repo:  line[2],
+			ID:    it.CurrentLineNumber,
+			Group: record[1],
+			Repo:  record[2],
 
 			DateString:  date.Format(getDateLayoutForTimeUnit(TimeUnitDays)),
 			MonthString: date.Format(getDateLayoutForTimeUnit(TimeUnitMonths)),
