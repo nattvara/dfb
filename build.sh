f19f0de75301c26234d699e2a07648c441ac45bc
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2019-08-04 00:28:56 +0200

    update build and install scripts to package app in an .app bundle

--- build.sh
+++ build.sh
@@ -11,6 +11,8 @@ if [ -f $OUT ]; then
     rm $OUT
 fi
 
+printf "creating dfb script... "
+
 touch $OUT
 echo "#!/usr/bin/env bash" > $OUT
 cat main.sh >> $OUT && printf "\n" >> $OUT
@@ -27,7 +29,28 @@ echo 'main "$@"' >> $OUT
 
 chmod +x $OUT
 
+echo "done"
+printf "compiling binaries... "
+
 go build -o ./build/dfb-progress-parser -i ./tools/progress-parser/cmd.go
-go build -o ./build/dfb-progress-parser-gui -i ./tools/progress-parser-gui/cmd.go
+FYNE_FONT=/Applications/dfb.app/Contents/Resources/fonts/Lato-Black.ttf go build -o ./build/dfb-progress-parser-gui -i ./tools/progress-parser-gui/cmd.go
 go build -o ./build/dfb-stats -i ./tools/stats/cmd.go
 go build -o ./build/dfb-fsd -i ./agents/fsd.go
+
+echo "done"
+printf "packaging application... "
+
+fyne package -executable build/dfb-progress-parser-gui -icon resources/icon.png -name dfb
+
+mkdir dfb.app/Contents/Resources/fonts
+cp resources/fonts/*.ttf dfb.app/Contents/Resources/fonts
+
+mkdir dfb.app/Contents/Resources/bin
+cp build/dfb dfb.app/Contents/Resources/bin/dfb
+cp build/dfb-progress-parser dfb.app/Contents/Resources/bin/dfb-progress-parser
+cp build/dfb-stats dfb.app/Contents/Resources/bin/dfb-stats
+cp build/dfb-fsd dfb.app/Contents/Resources/bin/dfb-fsd
+echo "FYNE_FONT=/Applications/dfb.app/Contents/Resources/fonts/Lato-Regular.ttf /Applications/dfb.app/Contents/MacOS/dfb-progress-parser-gui" > dfb.app/Contents/Resources/bin/dfb-progress-parser-gui
+chmod +x dfb.app/Contents/Resources/bin/dfb-progress-parser-gui
+
+echo "done"
--- install.sh
+++ install.sh
@@ -1,17 +1,47 @@
 #!/usr/bin/env bash
 
 launch_agent="$HOME/Library/LaunchAgents/com.dfb.fsd.plist"
+app_path="/Applications/dfb.app"
+bins_path="$app_path/Contents/Resources/bin"
+symlink_target="/usr/local/bin"
 
 if [ -f $launch_agent ]; then
     launchctl stop $launch_agent
     launchctl unload $launch_agent
 fi
 
-cp build/dfb /usr/local/bin/dfb
-cp build/dfb-progress-parser /usr/local/bin/dfb-progress-parser
-cp build/dfb-progress-parser-gui /usr/local/bin/dfb-progress-parser-gui
-cp build/dfb-stats /usr/local/bin/dfb-stats
-cp build/dfb-fsd /usr/local/bin/dfb-fsd
+if [ -d $app_path ]; then
+    echo "removing old version"
+    rm -r $app_path
+fi
+
+mv dfb.app /Applications
+
+if [ -f "$symlink_target/dfb" ]; then
+    rm "$symlink_target/dfb"
+fi
+
+if [ -f "$symlink_target/dfb-progress-parser" ]; then
+    rm "$symlink_target/dfb-progress-parser"
+fi
+
+if [ -f "$symlink_target/dfb-progress-parser-gui" ]; then
+    rm "$symlink_target/dfb-progress-parser-gui"
+fi
+
+if [ -f "$symlink_target/dfb-stats" ]; then
+    rm "$symlink_target/dfb-stats"
+fi
+
+if [ -f "$symlink_target/dfb-fsd" ]; then
+    rm "$symlink_target/dfb-fsd"
+fi
+
+ln -s "$bins_path/dfb" "$symlink_target/dfb"
+ln -s "$bins_path/dfb-progress-parser" "$symlink_target/dfb-progress-parser"
+ln -s "$bins_path/dfb-progress-parser-gui" "$symlink_target/dfb-progress-parser-gui"
+ln -s "$bins_path/dfb-stats" "$symlink_target/dfb-stats"
+ln -s "$bins_path/dfb-fsd" "$symlink_target/dfb-fsd"
 
 if [ ! -d "$HOME/.dfb.logs" ]; then
     echo "creating logs directory at $HOME/.dfb.logs"
