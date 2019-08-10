9234ebff2e6e8cebcf1d1d3f3a03a0981cd56d79
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2019-08-10 18:44:02 +0200

    fix docblock line breaks

--- tools/progress-parser-gui/cmd.go
+++ tools/progress-parser-gui/cmd.go
@@ -1,7 +1,6 @@
-// progress-gui will launch a gui window to display the gui of
-// a backup.
-// progress gui will parse messages from dfb and restic on stdin like the
-// following:
+// progress-gui will launch a gui window to display the gui of a backup.
+//
+// progress gui will parse messages from dfb and restic on stdin like the following:
 //
 // {"message_type":"dfb","action":"begin","group":"some-group","domain":"some-domain"}
 // {"message_type":"summary","files_new":1,"files_changed":2,"files_unmodified":83,"dirs_new":0,"dirs_changed":0,"dirs_unmodified":0,"data_blobs":0,"tree_blobs":0,"data_added":0,"total_files_processed":83,"total_bytes_processed":43535,"total_duration":0.388768151,"snapshot_id":"xxx"}
